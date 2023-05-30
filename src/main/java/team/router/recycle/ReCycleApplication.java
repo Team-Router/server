02c816bb0a3d94758ff50ddbe0428b8affe76a5e
HyunseokCheong <hyunseokcheong@gmail.com>
2023-05-30 20:07:30 +0900

    feat: first commit

---
+++ src/main/java/team/router/recycle/ReCycleApplication.java
@@ -0,0 +1,13 @@
+package team.router.recycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/router/recycle/ReCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package team.router.recycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
