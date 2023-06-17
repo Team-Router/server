5b98681a71d43fe3bdafa085ff3ff3b24611e3c0
hyunseokcheong <hyunseokcheong@gmail.com>
2023-06-17 20:00:48 +0900

    fix: cors config test

---
+++ src/main/java/team/router/recycle/config/WebConfig.java
@@ -0,0 +1,19 @@
+package team.router.recycle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://re-cycle-test.vercel.app")
+                .allowedMethods(HttpMethod.POST.name())
+                .allowedMethods(HttpMethod.GET.name());
+    }
+}
