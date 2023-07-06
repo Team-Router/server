c6562fcb1c785d36b86d4c5739f8b2451cd26769
doxxx <doxxx93@gmail.com>
2023-07-06 15:23:39 +0900

    fix: health

--- src/main/java/team/router/recycle/web/health/HealthController.java
+++ src/main/java/team/router/recycle/web/health/HealthController.java
@@ -1,20 +1,15 @@
 package team.router.recycle.web.health;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team.router.recycle.web.route.RouteRequest;
 
 @RestController
-@RequestMapping("/health-check")
-public class healthService {
+@RequestMapping("/health")
+public class HealthController {
     @GetMapping
-    public ResponseEntity<String> healthCheck() {
+    public ResponseEntity<String> health() {
         return ResponseEntity.ok("서비스가 정상적으로 작동하고 있습니다.");
     }
-
 }
