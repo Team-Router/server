156c05273e4a34d551606ee706a34c9919108873
doxxx <doxxx93@gmail.com>
2023-07-09 15:39:59 +0900

    fix: change realtime station to post

--- src/main/java/team/router/recycle/web/station/StationController.java
+++ src/main/java/team/router/recycle/web/station/StationController.java
@@ -1,7 +1,6 @@
 package team.router.recycle.web.station;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +10,19 @@ import team.router.recycle.domain.station.StationService;
 @RestController
 @RequestMapping("/station")
 public class StationController {
-    
+
     private final StationService stationService;
-    
+
     public StationController(final StationService stationService) {
         this.stationService = stationService;
     }
-    
+
     @PostMapping("/init")
     public ResponseEntity<?> initStation() {
         return stationService.initStation();
     }
-    
-    @GetMapping("/realtime")
+
+    @PostMapping("/realtime")
     public ResponseEntity<?> getRealtimeStation(@RequestBody StationRequest.RealtimeStationRequest realtimeStationRequest) {
         return stationService.getRealtimeStation(realtimeStationRequest);
     }
