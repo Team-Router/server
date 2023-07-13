092534f3d4746ef15efbf90ab0ebf7f6cae95a3b
hyunseokcheong <hyunseokcheong@gmail.com>
2023-07-13 09:49:43 +0900

    favorite place type

--- src/main/java/team/router/recycle/domain/favorite_place/FavoritePlaceRepository.java
+++ src/main/java/team/router/recycle/domain/favorite_place/FavoritePlaceRepository.java
@@ -4,11 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavoritePlaceRepository extends JpaRepository<FavoritePlace, Long> {
     
-    boolean existsFavoritePlaceByLatitudeAndLongitudeAndMemberId(Double latitude, Double longitude, Long memberId);
+    boolean existsFavoritePlaceByLatitudeAndLongitudeAndMemberIdAndType(Double latitude, Double longitude, Long memberId, FavoritePlace.Type type);
     
     List<FavoritePlace> findAllByMemberId(Long memberId);
+    
+    Optional<FavoritePlace> findByType(FavoritePlace.Type type);
 }
