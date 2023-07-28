01d8e6909e8e54fd438f556d0e51d040ea13cea1
doxxx <doxxx93@gmail.com>
2023-07-28 16:05:30 +0900

    feat: save unique refresh token

--- src/main/java/team/router/recycle/domain/token/TokenRepository.java
+++ src/main/java/team/router/recycle/domain/token/TokenRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
-    Optional<Token> findFirstByKeyOrderByIdDesc(String token);
+    Optional<Token> findByKey(String key);
 }
--- src/main/java/team/router/recycle/domain/token/TokenService.java
+++ src/main/java/team/router/recycle/domain/token/TokenService.java
@@ -2,6 +2,4 @@ package team.router.recycle.domain.token;
 
 public interface TokenService {
     Token issueToken(String key, String value);
-
-    Token findToken(String key);
 }
